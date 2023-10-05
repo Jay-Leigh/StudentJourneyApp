62052aa3ab28b77262735e6b95cd38a9157ce770
jaylegh.reece <vergotinejayleigh@gmail.com>
2023-10-05 18:21:42 +0200

    Some Pages for Student Journey App

---
+++ app/src/main/java/com/example/studentjourney/AlumniActivity.kt
@@ -0,0 +1,11 @@
+package com.example.studentjourney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlumniActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alumni)
+    }
+}
