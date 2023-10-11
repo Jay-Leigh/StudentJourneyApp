2165de860a7d1926f91ce87ae2b3e0a3d96a5f9b
jaylegh.reece <vergotinejayleigh@gmail.com>
2023-10-11 21:25:15 +0200

    Nav Updated - grey and smaller

--- app/src/main/java/com/example/studentjourney/HomeActivity.kt
+++ app/src/main/java/com/example/studentjourney/HomeActivity.kt
@@ -2,10 +2,24 @@ package com.example.studentjourney
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bttmNav = findViewById<BottomNavigationView>(R.id.Nav)
+        val navController = findNavController(R.id.llMain)
+
+        /*val appBarConfiguration = AppBarConfiguration(setOf(R.id.))*/
+
+        /*setupActionBarWithNavController(navController,appBarConfiguration)*/
+
+        bttmNav.setupWithNavController(navController)
     }
 }
