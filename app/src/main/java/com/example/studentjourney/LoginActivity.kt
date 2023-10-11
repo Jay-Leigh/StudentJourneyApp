e0b6c4211771c59f8d8cb0320772d9e0948583a6
jaylegh.reece <vergotinejayleigh@gmail.com>
2023-10-11 21:56:45 +0200

    Added Intents

--- app/src/main/java/com/example/studentjourney/LoginActivity.kt
+++ app/src/main/java/com/example/studentjourney/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.studentjourney
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val tvReg = findViewById<TextView>(R.id.tvCA)
+        tvReg.setOnClickListener {
+            val intent = Intent(this, ::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/studentjourney/MainActivity.kt
+++ app/src/main/java/com/example/studentjourney/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.studentjourney
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.Btnlogin)
+        val btnReg = findViewById<Button>(R.id.BtnReg)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnReg.setOnClickListener {
+            val intent = Intent(this, ::class.java)
+            startActivity(intent)
+        }
     }
 }
