1278e18f34c43c6e7fbe02f2969c7ef6739398f9
jaylegh.reece <vergotinejayleigh@gmail.com>
2023-10-14 15:56:53 +0200

    Register Authentication Complete

--- app/src/main/java/com/example/studentjourney/RegisterActivity.kt
+++ app/src/main/java/com/example/studentjourney/RegisterActivity.kt
@@ -4,11 +4,48 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
+import com.example.studentjourney.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRegisterBinding
+    private lateinit var firebaseAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.tvHaveAc.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnReg.setOnClickListener {
+            val fname = binding.etFname.text.toString()
+            val sname = binding.etSname.text.toString()
+            val email = binding.etEmail.text.toString()
+            val pass = binding.etPassw.text.toString()
+
+            if(fname.isNotEmpty() && sname.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty()){
+                firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Fields Cannot Be Empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
         val ibBack = findViewById<ImageButton>(R.id.ibBack)
         ibBack.setOnClickListener{
