b8087b5a95ab0dbeb14207e0921cd8b2d4dc8258
Matt-GitHubs <matthew.joseph.xpndble@gmail.com>
2023-10-20 01:01:24 +0200

    Splash + Audio

--- app/src/main/java/com/example/studentjourney/MainActivity.kt
+++ app/src/main/java/com/example/studentjourney/MainActivity.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //SplashScreen
+        Thread.sleep(4000)
+        installSplashScreen()
 
         val btnLogin = findViewById<Button>(R.id.Btnlogin)
         val btnReg = findViewById<Button>(R.id.BtnReg)
