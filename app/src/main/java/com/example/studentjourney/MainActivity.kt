a150d7768fe1ce4b5cd8886a22428d6b2c8029a1
Matt-GitHubs <matthew.joseph.xpndble@gmail.com>
2023-10-20 13:23:52 +0200

    Drawer related files

--- app/src/main/java/com/example/studentjourney/MainActivity.kt
+++ app/src/main/java/com/example/studentjourney/MainActivity.kt
@@ -3,8 +3,14 @@ package com.example.studentjourney
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.studentjourney.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +33,9 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        //Sign Out
+
+
     }
 }
